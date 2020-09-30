3c4ef5fb36cd5efe68f71af59a279c5d5081c869
Bilqisu Ismail <ismail.bilkisu@gmail.com>
2020-09-30 23:21:29 +0100

    calculate lowest terms

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,4 +1,14 @@
 ## TODO: complete the function "lowest_terms" below
 
-def lowest_terms(x):
-	return ""
+def lowest_terms(num, denom):
+	greatest_common_denom = 0
+    if denom == 0:
+        print("Division by zero")
+    return denom
+    
+    for i in range(min(abs(num), abs(denom)), 1, -1):
+            if num % i == 0 and denom % i == 0:
+                greatest_common_denom = i
+                break
+    return (num // greatest_common_denom,  denom // greatest_common_denom)
+	
