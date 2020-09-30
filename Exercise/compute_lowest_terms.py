b44170e6f68d87610a91df55ec817defd807f383
Bilqisu Ismail <ismail.bilkisu@gmail.com>
2020-10-01 00:13:39 +0100

    Well formatted output

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,14 +1,15 @@
 ## TODO: complete the function "lowest_terms" below
-
 def lowest_terms(num, denom):
-	greatest_common_denom = 0
+	# Testing denominator value equals zero
+    greatest_common_denom = 0
     if denom == 0:
         print("Division by zero")
-    return denom
-    
+        return denom
+    # calculate the greatest common denominator
     for i in range(min(abs(num), abs(denom)), 1, -1):
             if num % i == 0 and denom % i == 0:
                 greatest_common_denom = i
                 break
-    return (num // greatest_common_denom,  denom // greatest_common_denom)
-	
+	# divide the fraction		
+    return f'({num // greatest_common_denom}{"/"}{denom // greatest_common_denom})'
+print(lowest_terms(50, 25))
