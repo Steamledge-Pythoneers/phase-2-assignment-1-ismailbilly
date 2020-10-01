a7ec2e49be33b38741311119ca89bb075f713913
Bilqisu Ismail <ismail.bilkisu@gmail.com>
2020-10-01 07:39:53 +0100

    Making function lowest_term take only one argument

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,15 +1,27 @@
 ## TODO: complete the function "lowest_terms" below
-def lowest_terms(num, denom):
+#x = str(input('pls input the fraction: '))
+def lowest_terms(x):
 	# Testing denominator value equals zero
-    greatest_common_denom = 0
+    try:
+        return float(x)
+    except ValueError:
+        n, d = x.split('/')
+        try:
+            leading, n = n.split(' ')
+           
+        except ValueError: 
+            
+            greatest_common_denom = 0
+    num = int(n)
+    denom = int(d)
     if denom == 0:
-        print("Division by zero")
-        return denom
+        #print("Division by zero")
+        return "Undefined"
     # calculate the greatest common denominator
     for i in range(min(abs(num), abs(denom)), 1, -1):
             if num % i == 0 and denom % i == 0:
                 greatest_common_denom = i
                 break
 	# divide the fraction		
-    return f'({num // greatest_common_denom}{"/"}{denom // greatest_common_denom})'
-print(lowest_terms(50, 25))
+    return f'("{num // greatest_common_denom}{"/"}{denom // greatest_common_denom}")'
+#print(lowest_terms("10/2"))
